110f7ff125d88eee23adeed890489d7651b967a5
Dmitry <markelov16@gmail.com>
2023-02-20 19:35:06 +0500

    добавлен метод быстрого возведения в степень

--- dz_2.java
+++ dz_2.java
@@ -4,7 +4,23 @@ import java.io.FileWriter;
 import java.util.Scanner;
 
 public class dz_2 {
-
+    static int PowNums(int args[]){
+        int  result = 1;
+        int deg = args[1];
+        int num = args[0];
+        while(deg!=0) {
+            if (deg % 2 == 0) {
+                deg /= 2;
+                num *= num;
+            }
+            else {
+                deg--;
+                result *= num;
+            }
+        }
+        return result;
+    }
+    
     static int[] ReadFile() throws FileNotFoundException{ 
         int [] array= new int [2];
         int i = 0;
